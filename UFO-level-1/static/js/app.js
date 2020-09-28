80124adee1a0540da896096b3523c737bea74fc4
Mona Lisa <mario@Marios-MacBook-Air.local>
2020-09-28 17:40:55 -0500

    load

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,38 @@
-// from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// display table
+var tbody = d3.select('tbody');
+tableData.forEach(item => {
+    var tr = tbody.append('tr');
+    tr.append('td').text(item.datetime);
+    tr.append('td').text(item.city);
+    tr.append('td').text(item.state);
+    tr.append('td').text(item.country);
+    tr.append('td').text(item.shape);
+    tr.append('td').text(item.durationMinutes);
+    tr.append('td').text(item.comments);
+});
+
+// select button
+var button = d3.select('#filter-btn');
+
+// button filtering function
+button.on('click', function() {
+    d3.event.preventDefault();
+    var dateInput = d3.select('#datetime');
+    var dateValue = dateInput.property('value');
+
+    var filtered = tableData.filter(item => item.datetime === dateValue)
+    tbody.html(``);
+
+    filtered.forEach( item => {
+        var tr = tbody.append('tr');
+        tr.append('td').text(item.datetime);
+        tr.append('td').text(item.city);
+        tr.append('td').text(item.state);
+        tr.append('td').text(item.country);
+        tr.append('td').text(item.shape);
+        tr.append('td').text(item.durationMinutes);
+        tr.append('td').text(item.comments);
+    });
+});
